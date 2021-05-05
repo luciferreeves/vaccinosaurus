90615dc1e660e4d19b203a274eb5fd561ff14399
Priyansh <bobbyskhs@gmail.com>
2021-05-05 19:02:44 +0530

    Basic App

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors")({origin: true});
+const app = express();
+const port = 3000;
+
+app.use(cors);
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
