5cff7ced6676271380c59b7f8dcf1c2519ce1c83
Priyansh <bobbyskhs@gmail.com>
2021-05-05 19:08:30 +0530

    Added Environment variable for Port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors")({origin: true});
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors);
 app.use(express.static("public"));
