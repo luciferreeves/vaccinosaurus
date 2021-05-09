b6b3550f7f1816c2d65494aa0ed52756b2b59a1a
rohitsingh11 <rohit@twodotseven.com>
2021-05-09 16:22:21 +0530

    responsive navbar

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,69 +1,81 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDFSOXNMBmF4yXEFuowvdl_y-M0LbZoTGg",
-    authDomain: "vaccinosaurus.firebaseapp.com",
-    projectId: "vaccinosaurus",
-    storageBucket: "vaccinosaurus.appspot.com",
-    messagingSenderId: "349629911678",
-    appId: "1:349629911678:web:f7a12eaccd9e0b5bc87e2f"
+  apiKey: "AIzaSyDFSOXNMBmF4yXEFuowvdl_y-M0LbZoTGg",
+  authDomain: "vaccinosaurus.firebaseapp.com",
+  projectId: "vaccinosaurus",
+  storageBucket: "vaccinosaurus.appspot.com",
+  messagingSenderId: "349629911678",
+  appId: "1:349629911678:web:f7a12eaccd9e0b5bc87e2f",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 // if (window.location.pathname !== '/') {
-checkForLogin()
+checkForLogin();
 // }
-
 function checkForLogin() {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            if (window.location.pathname !== '/account' && window.location.pathname !== '/') {
-                window.location = `${window.location.origin}/account`;
-            }
-            if (document.getElementById('notifyMeButton')) {
-                document.getElementById('notifyMeButton').innerHTML = 'My Account';
-            }
-        } else {
-            if (window.location.pathname !== '/notify' && window.location.pathname !== '/') {
-                window.location = `${window.location.origin}/notify`;
-            }
-        }
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      if (
+        window.location.pathname !== "/account" &&
+        window.location.pathname !== "/"
+      ) {
+        window.location = `${window.location.origin}/account`;
+      }
+      if (document.getElementById("notifyMeButton")) {
+        document.getElementById("notifyMeButton").innerHTML = "My Account";
+        document.getElementById("logout-button").style.display = "block";
+      }
+    } else {
+      if (
+        window.location.pathname !== "/notify" &&
+        window.location.pathname !== "/"
+      ) {
+        window.location = `${window.location.origin}/notify`;
+      }
+    }
+  });
 }
 
 function myAccount() {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            if (window.location.pathname !== '/account') {
-                window.location = `${window.location.origin}/account`;
-            }
-        } else {
-            if (window.location.pathname !== '/notify') {
-                window.location = `${window.location.origin}/notify`;
-            }
-        }
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      if (window.location.pathname !== "/account") {
+        window.location = `${window.location.origin}/account`;
+      }
+    } else {
+      if (window.location.pathname !== "/notify") {
+        window.location = `${window.location.origin}/notify`;
+      }
+    }
+  });
 }
 
 function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-            var token = credential.accessToken;
-            var user = result.user;
-            window.location = `${window.location.origin}/account`;
-        }).catch((error) => {
-            var errorMessage = error.message;
-            alert(errorMessage);
-        });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+      var token = credential.accessToken;
+      var user = result.user;
+      window.location = `${window.location.origin}/account`;
+    })
+    .catch((error) => {
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
 }
 
 function logout() {
-    firebase.auth().signOut().then(() => {
-        window.location = `${window.location.origin}`;
-    }).catch((error) => {
-        alert('Error: Unable to log out.');
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      window.location = `${window.location.origin}`;
+    })
+    .catch((error) => {
+      alert("Error: Unable to log out.");
     });
 }
